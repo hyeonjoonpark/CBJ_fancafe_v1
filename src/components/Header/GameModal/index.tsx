ef9a9d26c3836cb886a4e37291a5deb37b03461c
hyeonjoonpark <pjjoon1379@gmail.com>
2023-12-13 01:13:10 +0900

    FEAT: 팬게임 링크 박제(?)

--- src/components/Header/GameModal/index.tsx
+++ src/components/Header/GameModal/index.tsx
@@ -3,11 +3,16 @@ import * as _ from "./style";
 import { Link } from "react-router-dom";
 
 const GameModal = () => {
+  const notion = () => {
+    window.open(
+      "https://tarry-halloumi-493.notion.site/BFS-73de61255cab4cac94264c2d302d3df2"
+    );
+  };
   return (
     <div>
       <Link to="#">
         <_.ModalWrapper>
-          <_.GameTitle>BBamjune Fan Studio</_.GameTitle>
+          <_.GameTitle onClick={notion}>BBamjune Fan Studio</_.GameTitle>
         </_.ModalWrapper>
       </Link>
 
