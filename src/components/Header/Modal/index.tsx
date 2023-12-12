7df38e026935b76cd9c1b3c9df6d34122f9186f8
hyeonjoonpark <pjjoon1379@gmail.com>
2023-12-13 01:06:01 +0900

    DELETE: ChatPage

--- src/components/Header/Modal/index.tsx
+++ src/components/Header/Modal/index.tsx
@@ -6,6 +6,7 @@ const Modal = () => {
   const { handleLogout } = useAuth();
   return (
     <div>
+      <_.ModalWrapper>내 정보</_.ModalWrapper>
       <_.ModalWrapper onClick={handleLogout}>로그아웃</_.ModalWrapper>
     </div>
   );
