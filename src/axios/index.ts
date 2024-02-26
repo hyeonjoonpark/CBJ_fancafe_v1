0b53245020e09a445f65cefcc29ab70a71a13d9a
hyeonjoonpark <pjjoon1379@gmail.com>
2024-02-26 21:41:44 +0900

    ubuntu path

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://115.140.87.3/api",
+  baseURL: "http://115.140.87.3:8080/api",
   headers: {
     "Content-Type": "application/json",
     withCredentials: true,
